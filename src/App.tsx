47f990efab3edad63b627328572dffdf2c04d2d4
Magamed-Ali <80570590+Magamed-Ali@users.noreply.github.com>
2023-03-14 16:42:15 +0300

    add examination wednesday

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,87 @@
-import React, {useState} from "react";
+
+import {combineReducers, createStore} from "redux";
+import {Provider, useDispatch, useSelector} from 'react-redux'
+import React, {useState, useReducer, useEffect} from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
 
 function App() {
 
+    const students = {
+        students: [
+            {id: 1, name: 'Bob'},
+            {id: 2, name: 'Alex'},
+            {id: 3, name: 'Donald'},
+            {id: 4, name: 'Ann'},
+        ]
+    }
+    type RemoveStudentAT = {
+        type: "REMOVE-STUDENT"
+        id: number
+    }
+    const RemoveStudentAC = (id: number): RemoveStudentAT => ({
+        type: "REMOVE-STUDENT",
+        id
+    })
+
+    const studentsReducer = (state = students, action: RemoveStudentAT) => {
+        switch (action.type) {
+            case "REMOVE-STUDENT":
+                return {
+                    ...state,
+                    students: state.students.filter(s => s.id !== action.id)
+                }
+        }
+        return state
+    }
+
+    const store = createStore(studentsReducer)
+    type RootStateType = ReturnType<typeof studentsReducer>
+
+
+    const StudentList = () => {
+        const listItemStyles = {
+            width: "100px",
+            borderBottom: "1px solid gray",
+            cursor: "pointer",
+        }
+        const students = useSelector((state: RootStateType) => state.students)
+        const dispatch = useDispatch()
+        const studentsList = students.map(s => {
+            const removeStudent = () => {
+                dispatch(RemoveStudentAC( s.id))
+            }
+            return (
+                <li key={s.id}
+                    style={listItemStyles}
+                    onClick={removeStudent}>
+                    {s.name}
+                </li>)
+        })
+        return (
+            <ol>
+                {studentsList}
+            </ol>
+
+        )
+    }
+
+
+    ReactDOM.render(<div>
+            <Provider store={store}>
+                <StudentList/>
+            </Provider>
+        </div>,
+        document.getElementById('root')
+    )
+
+
+
     return (
         <div>
-            {
 
-            }
         </div>
     );
 }
 
-/*import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-
-
-const root = ReactDOM.createRoot(
-    document.getElementById('root') as HTMLElement
-);
-root.render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>
-);*/
 export default App;
